615095315c1deba4f43fc6f512d17199f0b0a516
Ken Chanseau Saint-Germain <ken.chanseau@yahoo.fr>
2019-02-19 18:40:35 -0500

    Added a MVC structure

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+const indexControllers = require('../controllers/index.js');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', indexControllers.index);
 
 module.exports = router;
