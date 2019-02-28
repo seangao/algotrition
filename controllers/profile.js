622f456111742299fba9bf1632e2e7ad0dcec7de
Lauren Liu <lauren.liu@yale.edu>
2019-02-27 19:35:01 -0500

    put content in collapsing templates in profile.pug

--- controllers/profile.js
+++ controllers/profile.js
@@ -3,9 +3,13 @@ var user = require('../models/user');
 function profile(req, res, next) {
   var user_id = 42;
   var user_info = user.get_info(user_id);
+  var user_calories;
+  var user_name = "John";
+
   res.render(
     'profile',
-    { title: 'My profile', user_id: user_id, user_info: user_info }
+    { title: 'My profile', user_id: user_id, user_info: user_info, user_name: user_name,
+    user_calories: user_calories }
   );
 }
 
