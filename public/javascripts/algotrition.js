7c6b9f6cf1cd245c07c1eb0303c9159bf32d013f
seangao <seancty@gmail.com>
2019-02-27 19:46:32 -0500

    toggleAddNutrReqs cleanup

--- public/javascripts/algotrition.js
+++ public/javascripts/algotrition.js
@@ -1,19 +1,14 @@
 function toggleAddNutrReqs() {
-  console.log('click');
-  if ($('#add-nutr-reqs').css('display') == 'block') {
-    $('#add-nutr-reqs').css('display', 'none');
+  $('#add-nutr-reqs').toggle();
+  if ($('#add-nutr-reqs').is(':hidden')) {
+    $('#add-nutr-reqs-link').text('More options');
   } else {
-    $('#add-nutr-reqs').css('display', 'block');
+    $('#add-nutr-reqs-link').text('Less options');
   }
 }
 
 $(document).ready( function() {
      $('#add-nutr-reqs-link').click(function() {
-       $('#add-nutr-reqs').toggle();
-       if ($('#add-nutr-reqs').is(':hidden')) {
-         $('#add-nutr-reqs-link').text('More options');
-       } else {
-         $('#add-nutr-reqs-link').text('Less options');
-       }
+       toggleAddNutrReqs();
      });
  });
