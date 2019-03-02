de5efd48271880fb4862072af43459ffcd90cac0
Ken Chanseau Saint-Germain <ken.chanseau@yahoo.fr>
2019-03-02 17:57:52 -0500

    Some nice cards

--- controllers/calendar.js
+++ controllers/calendar.js
@@ -3,24 +3,94 @@ function calendar(req, res, next) {
     {
       name: 'Monday',
       id: 1,
-      recipes: [
+      meals: [
         {
-          name: "Banana smoothie",
-          ingredients: ["Banana", "Water"]
+          name: "Breakfast",
+          id: 1,
+          recipes: [
+            {
+              id:1,
+              name: "Banana-strawberry smoothie",
+              ingredients: ["Banana", "Strawberry"]
+            },
+            {
+              id:2,
+              name: "Plain yoghurt",
+              ingredients: ["Yoghurt"]
+            },
+            {
+              id:3,
+              name: "Coffee with sugar",
+              ingredients: ["Coffee", "Sugar"]
+            }
+          ],
+          eaten: true
         },
         {
-          name: "Yoghurt",
-          ingredients: ["Milk"]
+          name: "Lunch",
+          id: 2,
+          recipes: [
+            {
+              id:4,
+              name: "Omelette",
+              ingredients: ["Eggs", "Salt", "Oil", "Pepper"]
+            },
+            {
+              id:5,
+              name: "Plain yoghurt",
+              ingredients: ["Yoghurt"]
+            }
+          ],
+          eaten: true
+        },
+        {
+          name: "Dinner",
+          id: 3,
+          recipes: [
+            {
+              id: 6,
+              name: "Salad",
+              ingredients: ["Tomato", "Lettuce", "Olive oil"]
+            },
+            {
+              id: 7,
+              name: "Pasta with chicken",
+              ingredients: ["Pasta", "Chicken", "Oil", "Parmesan"]
+            },
+            {
+              id: 8,
+              name: "Apple",
+              ingredients: ["Apple"]
+            },
+            {
+              id: 9,
+              name: "Banana",
+              ingredients: ["Banana"]
+            }
+          ],
+          active: true
         }
       ]
     },
     {
       name: 'Tuesday',
       id: 2,
-      recipes: [
+      meals: [
         {
-          name: "Sticky rice",
-          ingredients: ["Rice", "Water"]
+          name: "Breakfast",
+          id: 4,
+          recipes: [
+            {
+              id:10,
+              name: "Banana-strawberry smoothie",
+              ingredients: ["Banana", "Strawberry"]
+            },
+            {
+              id:11,
+              name: "Plain yoghurt",
+              ingredients: ["Yoghurt"]
+            }
+          ]
         }
       ]
     }
