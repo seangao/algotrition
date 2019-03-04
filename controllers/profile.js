7ae32595aece02f7489c2a788f0dfb1d5e80073c
Lauren Liu <lauren.liu@yale.edu>
2019-03-03 19:25:18 -0500

    Profile view no longer uses depreceated panel

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,15 +1,63 @@
 var user = require('../models/user');
 
 function profile(req, res, next) {
-  var user_id = 42;
-  var user_info = user.get_info(user_id);
-  var user_calories;
-  var user_name = "John";
+  // var user_id = 42;
+  // var user_info = user.get_info(user_id);
+  // var user_calories;
+  // var user_name = "John";
+
+  var user = [
+    {
+      name: "General",
+      id: 0,
+      items: [
+        {
+          name: "Name",
+          value: "John"
+        },
+        {
+          name: "Weight",
+          value: "180 pounds"
+        },
+        {
+          name: "Age",
+          value: "22"
+        },
+        {
+          name: "Gender",
+          value: "male"
+        }
+      ]
+    },
+    {
+      name: "Nutritional Information",
+      id: 1,
+      items: [
+        {
+          name: "Allergens",
+          value: "peanuts and soy"
+        },
+        {
+          name: "Calorie Preference",
+          value: "2000"
+        }
+      ]
+    },
+    {
+      name: "History",
+      id: 2,
+      items: [
+        {
+          name: "Previous plans",
+          value: "Link to plans"
+        }
+      ]
+    }
+  ]
 
   res.render(
     'profile',
-    { title: 'My profile', user_id: user_id, user_info: user_info, user_name: user_name,
-    user_calories: user_calories }
+    { title: 'My profile', user: user}
   );
 }
 
