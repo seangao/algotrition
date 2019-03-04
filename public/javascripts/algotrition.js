53728d87730a91deb125550268fc33eb88a7cad5
seangao <seancty@gmail.com>
2019-03-04 15:44:28 -0500

    basic generator carousel

--- public/javascripts/algotrition.js
+++ public/javascripts/algotrition.js
@@ -7,8 +7,19 @@ function toggleAddNutrReqs() {
   }
 }
 
+function toggleNutrOptions(element) {
+  if (element.is('.active')) {
+    element.removeClass('active');
+  } else {
+    element.addClass('active');
+  }
+}
+
 $(document).ready( function() {
      $('#add-nutr-reqs-link').click(function() {
        toggleAddNutrReqs();
+     });
+     $('.generator-options-item').click(function() {
+       toggleNutrOptions($(this));
      });
  });
