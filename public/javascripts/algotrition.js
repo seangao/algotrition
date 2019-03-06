95417ffe3b4e0160ca6b54d17a52b940cee634bd
seangao <seancty@gmail.com>
2019-03-06 12:49:13 -0500

    finished generator carousel

--- public/javascripts/algotrition.js
+++ public/javascripts/algotrition.js
@@ -15,9 +15,25 @@ function toggleNutrOptions(element) {
   }
 }
 
+function updateNutrOptions() {
+  $('.generator-options-item').each(function(i, obj) {
+    obj = $(obj);
+    var id = obj.attr('id');
+    var input_id = id.slice(0, -7);
+    if (obj.is('.active')) {
+      $('#' + input_id).show();
+    } else {
+      $('#' + input_id).hide();
+    }
+  });
+}
+
 $(document).ready( function() {
      $('#add-nutr-reqs-link').click(function() {
        toggleAddNutrReqs();
+     });
+     $('#generator-button-next').click(function() {
+       updateNutrOptions();
      });
      $('.generator-options-item').click(function() {
        toggleNutrOptions($(this));
