bfe5c36687a2d676179fdb54c8fbdb2d32fe7df5
Ken Chanseau Saint-Germain <ken.chanseau@yahoo.fr>
2019-03-24 19:33:27 -0400

    Intitiate active meal

--- models/optimizer.js
+++ models/optimizer.js
@@ -35,7 +35,7 @@ function populate_constraints(input_constraints){
 
 
 
-	
+
 	if('calories-min' in input_constraints){
 		output["energy"] = {"min":input_constraints["calories-min"]};
 	}
@@ -184,7 +184,7 @@ function populate_recipe_variables(constraints, input_constraints){
 		//loop over constraint list calling single_constraint
 		var all_constraints_available = true;
 		var j;
-		for(j=0;j<constraint_list.length;j++){	
+		for(j=0;j<constraint_list.length;j++){
 
 			//single_constraint(constraint_list[j],recipe,tempObj)
 			if(! single_constraint(constraint_list[j],recipe,tempObj)){
@@ -231,9 +231,9 @@ function populate_recipe_variables(constraints, input_constraints){
 					if (recipe[allergens[j]] == false){
 					restriction_friendly = false;
 				}
-				
+
 				}
-			
+
 			}
 
 		}
@@ -265,7 +265,7 @@ function single_constraint(constraint_name,recipe,tempObj){
 			return true;
 		}
 	} else if (constraint_name.substr(0,constraint_name.length-1) in recipe){
-		
+
 		if(recipe[constraint_name.substr(0,constraint_name.length-1)] != 'NaN'){
 			tempObj[constraint_name] = recipe[constraint_name.substr(0,constraint_name.length-1)]
 			return true;
@@ -276,7 +276,7 @@ function single_constraint(constraint_name,recipe,tempObj){
 	}
 }
 
-//Creates ints object within the solver object 
+//Creates ints object within the solver object
 function populate_ints(variables){
 
 	var ints = {};
@@ -310,7 +310,7 @@ function return_calendar(model,results){
 			}
 
 
-			
+
 			var single_recipe = {
 				'id': 1,
 				'name': model["variables"][keys[i]]["recipe_name"],
@@ -330,7 +330,7 @@ function return_calendar(model,results){
 				var meal = {'name':'Dinner', 'id':3,'recipes':[single_recipe]};
 				meals[2] = meal;
 			}
-			
+
 
 
 		}
@@ -338,8 +338,11 @@ function return_calendar(model,results){
 
 	var week = [{'name':'Day 1','id':1,'meals':meals}];
 
-	return week;
+	//Initiate the first meal of the first day as the active one
+	week[0].meals[0].active = true;
 	
+	return week;
+
 
 }
 
