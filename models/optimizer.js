c6ad49ad07659013a5581a2803edd8204b942159
Ken Chanseau Saint-Germain <ken.chanseau@yahoo.fr>
2019-03-24 18:57:34 -0400

    Go to next meal

--- models/optimizer.js
+++ models/optimizer.js
@@ -311,8 +311,21 @@ function write_calendar_file(path,calendar){
 	})
 }
 
+function increment_active_meal(path, calendar, eaten_day, eaten_meal){
+	var fs = require("fs");
+	calendar[eaten_day].meals[eaten_meal].eaten = true;
+	if (calendar[eaten_day].meals[++eaten_meal]) {
+		calendar[eaten_day].meals[eaten_meal].active = true;
+	}
+	else if (calendar[++eaten_day]) {
+		calendar[eaten_day].meals[0].active = true;
+	}
+	fs.writeFile(path,JSON.stringify(calendar),(err) => {
+		if(err) throw err;
+	})
+}
 
 
 
-
-module.exports = {optimization,return_calendar,write_calendar_file};
+module.exports = {
+	optimization,return_calendar,write_calendar_file,increment_active_meal};
