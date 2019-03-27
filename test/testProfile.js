3928c673c49e302b28bbcee630ad34b30d36c284
seangao <seancty@gmail.com>
2019-03-27 19:13:51 -0400

    added more profile tests

--- test/testProfile.js
+++ test/testProfile.js
@@ -18,5 +18,47 @@ describe('Models', function() {
       expect(user_model.get_info(0).calories).to.be.a('number');
     });
 
+    it('should return gender field', function() {
+      expect(user_model.get_info(0)).to.have.property('gender');
+    });
+
+    it('gender should be male / female', function() {
+      expect(user_model.get_info(0).gender).to.satisfy(function(x) {
+        return x == 'Male' || x == 'Female';
+      });
+    });
+
+    it('should return age', function() {
+      expect(user_model.get_info(0)).to.have.property('age');
+    });
+
+    it('the age field returned should be a number', function() {
+      expect(user_model.get_info(0).age).to.be.a('number');
+    });
+
+    it('should return weight', function() {
+      expect(user_model.get_info(0)).to.have.property('weight');
+    });
+
+    it('the weight field returned should be a number', function() {
+      expect(user_model.get_info(0).weight).to.be.a('number');
+    });
+
+    it('should return height (ft)', function() {
+      expect(user_model.get_info(0)).to.have.property('height_ft');
+    });
+
+    it('the height (ft) field returned should be a number', function() {
+      expect(user_model.get_info(0).height_ft).to.be.a('number');
+    });
+
+    it('should return height (in)', function() {
+      expect(user_model.get_info(0)).to.have.property('height_in');
+    });
+
+    it('the height (in) field returned should be a number', function() {
+      expect(user_model.get_info(0).height_in).to.be.a('number');
+    });
+
   });
 });
