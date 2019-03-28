c4ec5182594d30f34a1b65b076c87cb7f503ed3c
Ken Chanseau Saint-Germain <ken.chanseau@yahoo.fr>
2019-03-27 22:04:55 -0400

    postgres problems

--- models/profile.js
+++ models/profile.js
@@ -1,6 +1,6 @@
 async function searchUserbyID(db, id) {
    const stmt = `
-       SELECT username, age, height, weight FROM public."Users" WHERE
+       SELECT username, age, height, weight FROM users WHERE
        id = $1
    `;
    return db.oneOrNone(stmt, [id])
