722ef4182c145cdfc5802d800a53ea1b44ab79be
graysonhyc <graysonhyc@gmail.com>
2019-03-27 22:15:38 -0400

    Added back local database config plus description

--- config.js
+++ config.js
@@ -1,4 +1,7 @@
 module.exports = {
   port: process.env.PORT || 3000,
   databaseURL: process.env.DATABASE_URL
+  
+  /* For local database testing: COMMENT IT when attempt to connect heroku database, DON'T DELETE */
+  //databaseURL: "postgres://postgres:@localhost:5432/postgres",
 };
