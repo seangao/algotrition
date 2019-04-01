510582ccbed0188abd428c95d990e6ae9c2cc167
graysonhyc <graysonhyc@gmail.com>
2019-03-31 21:40:51 -0400

    Placeholder for updating profiles

--- models/profile.js
+++ models/profile.js
@@ -6,6 +6,15 @@ async function searchUserbyID(db, id) {
    return db.oneOrNone(stmt, [id])
 }
 
+async function updateProfile(db, placeholder, id) {
+    const stmt = `
+        UPDATE users
+        SET 
+        WHERE id = $1
+    `;
+    return db.oneOrNone(stmt, [id])
+}
+
 module.exports = {
     searchUserbyID
 };
