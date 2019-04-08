b319cf3617c84aa23e5c3550d1503c9ed7269e29
seangao <seancty@gmail.com>
2019-04-08 17:04:29 -0400

    should be fixed

--- models/sql.js
+++ models/sql.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 
 function readSQLFile(fileName) {
-  console.log(process.cwd());
   const query = fs.readFileSync(fileName).toString()
     .replace(/(\r\n|\n|\r)/gm, ' ') // remove newlines
     .replace(/\s+/g, ' '); // excess white space
