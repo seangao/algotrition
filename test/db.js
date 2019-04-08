388587dc4694fa2cbbae7af36919b03eedd55367
seangao <seancty@gmail.com>
2019-04-08 16:49:46 -0400

    create recipes_sp database

--- test/db.js
+++ test/db.js
@@ -7,19 +7,30 @@ const registerModel = require('../models/register');
 const loginModel = require('../models/login');
 const profileModel = require('../models/profile');
 const recipesModel = require('../models/recipes');
+const sqlModel = require('../models/sql');
 
 describe('db', () => {
+  // it('create users database', (done) => {
+  //   db.none(`
+  //     create table users (
+  //       id serial primary key,
+  //       username varchar (100) unique not null,
+  //       password varchar (100) not null,
+  //       age numeric,
+  //       height numeric,
+  //       weight numeric
+  //     );
+  //   `)
+  //     .then(() => done());
+  // });
+
   it('create users database', (done) => {
-    db.none(`
-      create table users (
-        id serial primary key,
-        username varchar (100) unique not null,
-        password varchar (100) not null,
-        age numeric,
-        height numeric,
-        weight numeric
-      );
-    `)
+    db.none(sqlModel.readSQLFile('users.sql'))
+      .then(() => done());
+  });
+
+  it('create spoonacular recipes database', (done) => {
+    db.none(sqlModel.readSQLFile('recipes_sp.sql'))
       .then(() => done());
   });
 
@@ -43,9 +54,7 @@ describe('db', () => {
 
   it('search user by id', async () => {
     const data = await profileModel.searchUserbyID(db, 1);
-    console.log(data);
-    expect(data).to.satisfy(d => d.id === 1
-        && d.username === 'test1'
+    expect(data).to.satisfy(d => d.username === 'test1'
         && d.age === '20'
         && d.weight === '120');
   });
