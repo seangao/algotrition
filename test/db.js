ec8c9ed466f49a8b76ffd710f308f946eb31161c
seangao <seancty@gmail.com>
2019-04-08 12:46:15 -0400

    rethrow err test

--- test/db.js
+++ test/db.js
@@ -1,15 +1,14 @@
-const { expect } = require('chai');
+const expect = require('chai').expect;
 
 const app = require('../app');
-
-const { db } = app.locals;
+const db = app.locals.db;
 const registerModel = require('../models/register');
 const loginModel = require('../models/login');
 const profileModel = require('../models/profile');
 const recipesModel = require('../models/recipes');
 
-describe('db', () => {
-  it('create users database', (done) => {
+describe ('db', function() {
+  it('create users database', function(done) {
     db.none(`
       create table users (
         id serial primary key,
@@ -20,59 +19,69 @@ describe('db', () => {
         weight numeric
       );
     `)
-      .then(() => done());
+    .then(() => done());
   });
 
-  it('insert new user manually', () => {
+  it('insert new user manually', function() {
     registerModel.insertNewUser(db, {
       username: 'test1',
       password: 'test',
       age: 20,
       weight: 120,
     })
-      .then((data) => {
-        expect(data.id).to.equal(1);
-      });
+    .then(data => {
+      expect(data.id).to.equal(1);
+    });
   });
 
-  it('search user by username', () => {
+  it('search user by username', function() {
     loginModel.searchUser(db, { username: 'test1' })
-      .then((data) => {
-        expect(data).to.satisfy(d => d.id === 1
-          && d.username === 'test1'
-          && d.age === 20
-          && d.weight === 120);
+    .then(data => {
+      expect(data).to.satisfy(function(d) {
+        return d.id === 1 &&
+          d.username === 'test1' &&
+          d.age === 20 &&
+          d.weight === 120;
       });
+    });
   });
 
-  it('search user by id', () => {
+  it('search user by id', function() {
     profileModel.searchUserbyID(db, 1)
-      .then((data) => {
-        expect(data).to.satisfy(d => d.id === 1
-          && d.username === 'test1'
-          && d.age === 20
-          && d.weight === 120);
+    .then(data => {
+      expect(data).to.satisfy(function(d) {
+        return d.id === 1 &&
+          d.username === 'test1' &&
+          d.age === 20 &&
+          d.weight === 120;
       });
+    });
   });
 
-  it('update profile', () => {
+  it('update profile', function() {
     profileModel.updateProfile(db, 1, {
       Name: 'test2',
       Weight: 140,
-      Age: 30,
+      Age: 30
     })
-      .then((data) => {
-        expect(data).to.satisfy(d => d.id === 1
-          && d.username === 'test2'
-          && d.age === 30
-          && d.weight === 140);
+    .then(data => {
+      expect(data).to.satisfy(function(d) {
+        return d.id === 1 &&
+          d.username === 'test2' &&
+          d.age === 30 &&
+          d.weight === 140;
       });
+    });
   });
 
-  it('get recipes', () => {
+  it('get recipes', function () {
     recipesModel.getAllRecipes(db)
-      .then((data) => {
-        expect(data).to.not.equal(null);
-      });
+    .then(data => {
+      expect(data).to.not.equal(null);
+    })
+    .catch(function (err) {
+      throw err;
+    });
   });
+
 });
