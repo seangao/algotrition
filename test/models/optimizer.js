b810fc2b55beb2f273afbdbdc4bc7a06571b4b28
graysonhyc <graysonhyc@gmail.com>
2019-04-10 19:06:46 -0400

    Started to test optimizer.js

---
+++ test/models/optimizer.js
@@ -0,0 +1,10 @@
+const { expect } = require('chai');
+
+const optimizerModel = require('../../models/optimizer');
+
+describe('models/optimizer', () =>) {
+    it('populateConstraints', () => {
+        expect(optimizerModel.populateConstraints().to.equal());
+    });
+    
+}
