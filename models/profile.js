de63d3f914281aac719d84174c3d4b6888b5a599
graysonhyc <graysonhyc@gmail.com>
2019-04-14 18:05:20 -0400

    Profile Fixed v2

--- models/profile.js
+++ models/profile.js
@@ -1,3 +1,13 @@
+function convertHeight(feet, inch) {
+  return feet * 30.48 + inch * 2.54;
+}
+
+function reverseHeight(cm) {
+  const feet = Math.floor(cm / 30.48);
+  const inch = Math.round((cm - feet * 30.48) / 2.54);
+  return [feet, inch];
+}
+
 async function searchUserbyID(db, id) {
   const stmt = `
        SELECT username, age, height, weight, gender FROM users WHERE
@@ -7,15 +17,16 @@ async function searchUserbyID(db, id) {
 }
 
 async function updateProfile(db, id, info) {
+  console.log(info)
   const stmt = `
         UPDATE users
-        SET username = '$2:value', weight = $3, age = $4, gender = $5
+        SET username = '$2:value', weight = $3, age = $4, gender = $5, height = $6
         WHERE id = $1
         RETURNING id, username, height, weight, age, gender
     `;
-  return db.oneOrNone(stmt, [id, info.Username, info.Weight, info.Age, info.Gender]);
+  return db.oneOrNone(stmt, [id, info.Username, info.Weight, info.Age, info.gender, convertHeight(info.ft, info.in)]);
 }
 
 module.exports = {
-  searchUserbyID, updateProfile,
+  searchUserbyID, updateProfile, convertHeight, reverseHeight
 };
--- routes/profile.js
+++ routes/profile.js
@@ -6,7 +6,6 @@ const bodyParser = require('body-parser');
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 const profileControllers = require('../controllers/profile');
-const loginControllers = require('../controllers/login.js');
 
 router.get('/', profileControllers.generateProfile);
 router.post('/updateprofile', urlencodedParser, profileControllers.updateProfile);
