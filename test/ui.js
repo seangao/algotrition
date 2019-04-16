65b8dfb5745780b70858a4154d6d06bd5157978c
seangao <seancty@gmail.com>
2019-04-16 13:59:13 -0400

    test

--- test/ui.js
+++ test/ui.js
@@ -26,9 +26,9 @@ describe('UI', () => {
     await driver.get(baseURL + '/generator');
     await driver.findElement(By.id('potassium-toggle')).click();
     await driver.findElement(By.id('generator-nutr-button')).click();
-    driver.manage().timeouts().implicitlyWait(1);
+    await driver.manage().setTimeouts().implicitlyWait(1000);
     await driver.findElement(By.id('generator-opt-button')).click();
-    driver.manage().timeouts().implicitlyWait(1);
+    await driver.manage().setTimeouts().implicitlyWait(1000);
     await driver.findElement(By.id('generate')).click();
     await driver.wait(until.elementLocated(By.id('plan-calendar')), 10000);
     const title = await driver.getTitle();
