094618ccd24684b67bb37a784a835880b3bca583
seangao <seancty@gmail.com>
2019-04-16 12:52:27 -0400

    test

--- test/ui.js
+++ test/ui.js
@@ -2,20 +2,16 @@ require('chromedriver');
 const { Builder, Key, By, until } = require('selenium-webdriver');
 const { assert } = require('chai');
 
+const baseURL = 'https://algotrition.herokuapp.com';
+
 describe('UI', () => {
   let driver;
   before(async () => {
     driver = await new Builder().forBrowser('chrome').build();
   });
 
-  it('test', async () => {
-    await driver.get('http://google.com');
-    const title = await driver.getTitle();
-    assert.equal(title, 'Google');
-  });
-
-  it('Load home', async () => {
-    await driver.get('http://localhost:3000/');
+  it('load home', async () => {
+    await driver.get(baseURL);
     // await driver.findElement(By.id('potassium-toggle')).click();
     const title = await driver.getTitle();
     assert.equal(title, 'Algotrition');
