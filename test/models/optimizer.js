fe08544d720dbf2a22a168621af0aed249cb9b80
shoemb <47543417+shoemb@users.noreply.github.com>
2019-04-21 14:52:01 -0400

    Tests for optimizer functionality

--- test/models/optimizer.js
+++ test/models/optimizer.js
@@ -1,10 +1,98 @@
 const { expect } = require('chai');
 
-const optimizerModel = require('../../models/optimizer');
+const app = require('../../app');
+const { db } = app.locals;
+const optimizer = require('../../models/optimizer');
+const recipesMod = require('../../models/recipes')
 
-// describe('models/optimizer', () =>) {
-//     it('populateConstraints', () => {
-//         expect(optimizerModel.populateConstraints().to.equal());
-//     });
-// 
-// }
+describe('models/optimizer', () => {
+    it('fitsConstraints', async () => {
+    	console.log('running');
+    	var inputConstraints = { 'calories-min': '1500',
+		  'calories-max': '2500',
+		  meals: '4',
+		  days: '3',
+		  'protein-min': '50',
+		  'protein-max': '100',
+		  'fat-min': '20',
+		  'fat-max': '100',
+		  'optimize-id': '0',
+		  generate: '' };
+
+		const recipes = await recipesMod.getAllRecipes(db);
+		const calendar = optimizer.optimization(inputConstraints, recipes);
+
+		var satisfied = true;
+		
+		var i;
+		var j;
+		var recipe;
+		for(i=0; i<calendar.length; i++) {
+			var totalCalories = 0;
+			var totalProtein = 0;
+			var totalFat = 0;
+			for(j=0; j<calendar[i].meals.length; j++){
+				recipe = await recipesMod.getSpoonRecipeById(db, calendar[i].meals[j].recipes[0].id)
+				totalProtein += recipe.protein*calendar[i].meals[j].recipes[0].servings;
+				totalCalories += recipe.energy*calendar[i].meals[j].recipes[0].servings;
+				totalFat += recipe.fat*calendar[i].meals[j].recipes[0].servings;
+			}
+			
+			if (totalProtein < inputConstraints['protein-min'] || totalProtein > inputConstraints['protein-max']){
+				satisfied = false;
+			}
+			if (totalFat < inputConstraints['fat-min'] || totalFat > inputConstraints['fat-max']){
+				satisfied = false;
+			}
+			if (totalCalories < inputConstraints['calories-min'] || totalCalories > inputConstraints['calories-max']){
+				satisfied = false;
+			}
+		}
+		
+        //expect(optimizerModel.populateConstraints().to.equal());
+        expect(satisfied).to.be.true;
+    }).timeout(10000);
+
+
+    it('optimizedCriteria', async () => {
+    	console.log('running');
+    	var inputConstraints = { 'calories-min': '1500',
+		  'calories-max': '2500',
+		  meals: '4',
+		  days: '3',
+		  'protein-min': '50',
+		  'protein-max': '100',
+		  'fat-min': '20',
+		  'fat-max': '100',
+		  'optimize-id': '0',
+		  generate: '' };
+
+		const recipes = await recipesMod.getAllRecipes(db);
+		const calendar = optimizer.optimization(inputConstraints, recipes);
+
+		var satisfied = true;
+		
+		var i;
+		var j;
+		var recipe;
+		var totalTimeArray = [];
+		for(i=0; i<calendar.length; i++) {
+			var totalTime = 0;
+			for(j=0; j<calendar[i].meals.length; j++){
+				recipe = await recipesMod.getSpoonRecipeById(db, calendar[i].meals[j].recipes[0].id)
+				totalTime += recipe["total_time_seconds"];
+			}
+			totalTimeArray.push(totalTime);
+		}
+
+		
+		for(i=1; i<totalTimeArray.length; i++){
+			if(totalTimeArray[i] < totalTimeArray[i-1]){
+				satisfied = false;
+			}
+		}
+
+        expect(satisfied).to.be.true;
+    }).timeout(10000);
+
+});
